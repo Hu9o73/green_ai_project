d0ecfcae2ee6d7fa4c7318ebc2adb07422158543
Hu9o73 <hbonnell@live.fr>
2024-11-27 16:21:51 +0100

    Added the data retriever

---
+++ dataRetriever.py
@@ -0,0 +1,33 @@
+import requests
+import pandas as pd
+
+def retrieveDataFromJson(url : str):
+    '''
+    Retrieve the json data of the given URL.
+    '''
+    response = requests.get(url)
+
+    if(response.status_code == 200):    
+        # Means that we could get the data
+        # Parse the JSON
+        data = response.json()
+
+        # Extract features
+        features = data.get("features", [])
+
+        # Flatten the data into a list of dictionaries
+        flattened_data = []
+        for feature in features:
+            # Combine properties and geometry into a single dictionary
+            flattened_dict = feature.get("properties", {})
+            geometry = feature.get("geometry", {})
+            flattened_dict["geometry"] = geometry
+            flattened_data.append(flattened_dict)
+
+        # Create a pandas DataFrame
+        dataFrame = pd.DataFrame(flattened_data)
+
+        return dataFrame
+    else:
+        print("Error: couldn't GET url: ", url)
+        return None
